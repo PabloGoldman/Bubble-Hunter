dee596d1a0a994f65e8b87e829f86c6e74a7bf10
Guido Tello <lpmaleinn@gmail.com>
2021-11-28 21:56:37 -0300

    Updated Credits

--- src/scenes/credits/Credits.cpp
+++ src/scenes/credits/Credits.cpp
@@ -1,17 +1,62 @@
 #include "Credits.h"
 #include "SFML/Graphics.hpp"
+#include "general/externs/Externs.h"
+
 
 using namespace sf;
 
+void Credits::InitCredits()
+{
+	title = Text();
+	programmer1 = Text();
+	programmer2 = Text();
+
+	background = RectangleShape();
+	
+	background.setFillColor(Color::Black);
+	background.setSize(Vector2f(ExternVars::window.x, ExternVars::window.y));
+	background.setPosition(Vector2f(0, 0));
+
+	title.setCharacterSize(60);
+	programmer1.setCharacterSize(40);
+	programmer2.setCharacterSize(40);
+
+	title.setFillColor(Color::White);
+	programmer1.setFillColor(Color::White);
+	programmer2.setFillColor(Color::White);
+
+	title.setString("CREDITS");
+	programmer1.setString("Programmer: GOLDMAN, Pablo");
+	programmer2.setString("Programmer: TELLO, Guido");
+
+	title.setFont(ExternVars::font);
+	programmer1.setFont(ExternVars::font);
+	programmer2.setFont(ExternVars::font);
+
+	title.setPosition(Vector2f(ExternVars::window.x / 2 - title.getPosition().x / 2, ExternVars::window.y / 4));
+	programmer1.setPosition(Vector2f(ExternVars::window.x / 2 - title.getPosition().x / 2, ExternVars::window.y * 3 / 5));
+	programmer2.setPosition(Vector2f(ExternVars::window.x / 2 - title.getPosition().x / 2, ExternVars::window.y * 4 / 5));
+
+}
+
 void Credits::InCredits(sf::RenderWindow* window)
 {
-	CheckInput();
+	if (!inited)
+	{
+		InitCredits();
+		inited = true;
+	}
+	CheckInput(window);
 	DrawCredits(window);
+
 }
 
 void Credits::DrawCredits(sf::RenderWindow* window)
 {
-
+	window->draw(background);
+	window->draw(title);
+	window->draw(programmer1);
+	window->draw(programmer2);
 }
 
 void Credits::SetSceneManager(SceneManager* sm)
@@ -24,10 +69,21 @@ void Credits::ReturnToMenu()
 	scene->SetSceneManager(Scene::MENU);
 }
 
-void Credits::CheckInput()
+void Credits::CheckInput(sf::RenderWindow* window)
 {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
-	{
-		ReturnToMenu();
+	sf::Event event;
+
+	while (window->pollEvent(event)) {
+		if (event.type == sf::Event::Closed) {
+			window->close();
+		}
+		if (event.type == sf::Event::KeyPressed)
+		{
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+			{
+				ReturnToMenu();
+			}
+		}
 	}
+	
 }
--- src/scenes/credits/Credits.h
+++ src/scenes/credits/Credits.h
@@ -7,12 +7,20 @@ class Credits
 {
 private:
 	SceneManager* scene;
+	sf::Text title;
+	sf::Text programmer1;
+	sf::Text programmer2;
+	sf::RectangleShape background;
+
+	bool inited = false;
+
 public:
 
+	void InitCredits();
 	void InCredits(sf::RenderWindow* window);
 	void DrawCredits(sf::RenderWindow* window);
 	void SetSceneManager(SceneManager* sm);
 	void ReturnToMenu();
-	void CheckInput();
+	void CheckInput(sf::RenderWindow* window);
 };
 
